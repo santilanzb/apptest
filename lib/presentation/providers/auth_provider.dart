94ee11948c18483adfff7756146c35f370b78e47
santilanb <santilanb@gmail.com>
2025-11-02 01:06:25 -0400

    Fix tests and remove unused import

--- lib/presentation/providers/auth_provider.dart
+++ lib/presentation/providers/auth_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/models/user_model.dart';
 import '../../data/repositories/auth_repository.dart';
